adb6358eb26078b59af683974376d4c95abb2b33
bananak1n99 <lcvi.ktpm0217@student.ctuet.edu.vn>
2023-08-11 17:35:30 +0700

    5

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,24 @@
-import './stylesheets/alignments.css';
-import './stylesheets/theme.css';
-import './stylesheets/sizes.css';
-import './stylesheets/custom-components.css';
-import './stylesheets/formElements.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
 
+import "./stylesheets/alignments.css";
+import "./stylesheets/theme.css";
+import "./stylesheets/sizes.css";
+import "./stylesheets/custom-components.css";
+import "./stylesheets/formElements.css";
 
 function App() {
   return (
-    <div 
-    className="bg-primary flex justify-center items-center h-screen">
-      
-      <h1 className="text-white">Dzix's Library</h1>
-    
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" component={Login} />
+          <Route path="/register" component={Register} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
