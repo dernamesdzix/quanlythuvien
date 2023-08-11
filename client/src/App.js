8d61ac7f57e04527d41945ac35205407dff40227
bananak1n99 <lcvi.ktpm0217@student.ctuet.edu.vn>
2023-08-11 16:43:22 +0700

    3

---
+++ client/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Button, Space } from 'antd';
+
+function App() {
+  return (
+    <div>
+      <h1>Dzix's Library</h1>
+      <Space wrap>
+    <Button type="primary">Primary Button</Button>
+    <Button>Default Button</Button>
+    <Button type="dashed">Dashed Button</Button>
+    <Button type="text">Text Button</Button>
+    <Button type="link">Link Button</Button>
+  </Space>
+    </div>
+  );
+}
+
+export default App;
