4a1dbd2e9736bfa93fd13c4661e970cdc86535d7
bananak1n99 <lcvi.ktpm0217@student.ctuet.edu.vn>
2023-08-12 19:25:39 +0700

    done 5

---
+++ server/config/dbConfig.js
@@ -0,0 +1,15 @@
+const mongoose = require(`mongoose`);
+// moongoose.set(`strictQuery`, false);
+
+mongoose.connect(process.env.mongo_URL);
+
+const connection = mongoose.connection;
+
+connection.on(`connected`, () => {
+  console.log("Connected to MongoDB Suscessfully");
+});
+connection.on(`error`, () => {
+  console.log("Connected to MongoDB Failed");
+});
+
+module.exports = connection;
