d7195962be438a4312f5dd6076200efd12c74cf3
bananak1n99 <lcvi.ktpm0217@student.ctuet.edu.vn>
2023-08-30 22:50:35 +0700

    done 39

--- client/src/pages/Profile/BorrowedBooks/index.js
+++ client/src/pages/Profile/BorrowedBooks/index.js
@@ -1,7 +1,78 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { message, Modal, Table } from "antd";
 
-function BorrowedBooks() {
-  return <div>BorrowedBooks</div>;
+import { ShowLoading, HideLoading } from "../../../redux/loaderSlice";
+
+import { useDispatch, useSelector } from "react-redux";
+import moment from "moment";
+import Button from "../../../components/Button";
+import { GetIssues } from "../../../api/issue";
+
+function IssuedBooks() {
+  const { user } = useSelector((state) => state.users);
+  const [issuedBooks, setIssuedBooks] = React.useState([]);
+  const dispatch = useDispatch();
+  const getIssues = async () => {
+    try {
+      dispatch(ShowLoading());
+      const response = await GetIssues({
+        user: user._id,
+      });
+      dispatch(HideLoading());
+      if (response.success) {
+        setIssuedBooks(response.data);
+      }
+    } catch (error) {
+      dispatch(HideLoading());
+      message.error(error.message);
+    }
+  };
+
+  useEffect(() => {
+    getIssues();
+  }, []);
+
+  const columns = [
+    {
+      title: "Id",
+      dataIndex: "_id",
+    },
+    {
+      title: "Book",
+      dataIndex: "book",
+      render: (book) => book.title,
+    },
+    {
+      title: "Issued On",
+      dataIndex: "issueDate",
+      render: (issueDate) => moment(issueDate).format("DD-MM-YYYY hh:mm A"),
+    },
+    {
+      title: "Return Date (Due Date)",
+      dataIndex: "returnDate",
+      render: (dueDate) => moment(dueDate).format("DD-MM-YYYY hh:mm A"),
+    },
+    {
+      title: "Rent",
+      dataIndex: "rent",
+    },
+    {
+      title: "Fine",
+      dataIndex: "fine",
+    },
+    {
+      title: "Returned On",
+      dataIndex: "returnedDate",
+      render: (returnedDate) => {
+        if (returnedDate) {
+          return moment(returnedDate).format("DD-MM-YYYY hh:mm A");
+        } else {
+          return "Not Returned Yet";
+        }
+      },
+    },
+  ];
+  return <Table columns={columns} dataSource={issuedBooks} />;
 }
 
-export default BorrowedBooks;
+export default IssuedBooks;
